2de117077dd2bdb19f17d05c24275a8ee281a808
Daehwa Kim <cd5465@hanmail.net>
2025-08-09 10:00:28 +0900

    [Bronze II] Title: 진법 변환, Time: 180 ms, Memory: 17772 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String N = scanner.next();
+        int B = scanner.nextInt();
+
+        scanner.close();
+
+        int result = Integer.parseInt(N, B);
+
+        System.out.println(result);
+    }
+}
